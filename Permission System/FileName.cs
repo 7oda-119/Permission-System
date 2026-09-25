42a2d4de52fef19ed2c4891d332f0372164f63c7
7oda <mlieegy@gmail.com>
2026-09-25 21:45:13 +0300

    Finished

---
+++ Permission System/FileName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Permission_System
+{
+    [Flags]
+    internal enum Permission
+    {
+        None = 0,
+        Read = 1,
+        Write = 2,
+        Delete = 4,
+        Execute = 8
+    }
+}
--- Permission System/Program.cs
+++ Permission System/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter permissions: ");
+            string[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Permission permissions = Permission.None;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Permission p = Enum.Parse<Permission>(arr[i]);
+                permissions |= p;
+            }
+
+            Console.WriteLine($"Granted: {permissions} ({(int)permissions})");
+
+            Permission add = ReadPermision("Enter permission to add: ");
+            permissions |= add;
+            Console.WriteLine($"Add {add}: {permissions} ({(int)permissions})");
+
+            Permission revoke = ReadPermision("Enter permission to revoke: ");
+            permissions &= ~revoke;
+            Console.WriteLine($"Revoke {revoke}: {permissions} ({(int)permissions})");
+
+            Console.Write("Has:");
+            if((permissions & Permission.Read) !=0)
+                Console.Write(" Read");
+            if((permissions & Permission.Write) != 0)
+                Console.Write(" Write");
+            if((permissions & Permission.Delete) != 0)
+                Console.Write(" Delete");
+            if((permissions & Permission.Execute) != 0)
+                Console.Write(" Execute");
         }
+
+        static Permission ReadPermision(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+
+                if (Enum.TryParse<Permission>(Console.ReadLine(), ignoreCase: true, out Permission p))
+                    return p;
+            }
+        }
+
     }
+
+
 }
