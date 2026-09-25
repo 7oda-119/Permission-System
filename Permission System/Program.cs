4c0f37c9edbf6abe1db4c051203655d4fb071dcc
7oda <mlieegy@gmail.com>
2026-09-25 20:55:45 +0300

    Add project files.

---
+++ Permission System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Permission_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
